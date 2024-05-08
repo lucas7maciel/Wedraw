99104a805ea80554746a33044315542cb5681716
Lucas Maciel <lucasmacielcontato@gmail.com>
2024-05-08 00:43:06 -0300

    Feat: Setting sign up sections

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,46 +1,172 @@
-'use client'
+"use client";
 
-import { Name } from "./name";
-import { Password } from "./password";
-import { State } from "./state";
-import { Parallax } from "./parallax";
+// Sections
+import Intro from "@/app/signup/(sections)/Intro";
+import Username from "@/app/signup/(sections)/Username";
+import Email from "@/app/signup/(sections)/Email";
+import Registering from "@/app/signup/(sections)/Registering";
+// Hooks
+import { RefObject, useEffect, useRef, useState } from "react";
+// Types
+import type { SectionProps } from "../../types/Section.model";
 
-/*
-O que fazer:
-- Repetir animações               V (checar)
-- Checar inputs                   V (alterar)
-- Mensagens
-- Animações das mensagens         V
-- Voltar                          V
-- Gerenciar focus
-- Ver se pode scrollar
-
-- Paralax
-*/
-
+// This function doesnt contain any DOM elements, just the logic to navigate
+// through them, they're located in './(sections)'
 export default function SignUp() {
-  function scroll(section : string) {
-    const el: HTMLElement | null = document.getElementById(section);
+  // Current section index
+  const [currSec, setCurrSec] = useState<number>(0);
 
-    el?.scrollIntoView({
-      behavior: "smooth",
-    });
+  // Sections components and data
+  const sections: Array<SectionProps> = [
+    {
+      comp: Intro,
+      ref: useRef(),
+    },
+    {
+      comp: Username,
+      ref: useRef(),
+    },
+    {
+      comp: Email,
+      ref: useRef(),
+    },
+    {
+      comp: Registering,
+      ref: useRef(),
+    },
+  ];
+
+  //
+  function scrollInto(sec: number) {
+    const el: HTMLElement | null | undefined = sections[sec]?.ref.current;
+
+    if (el) {
+      (document.activeElement as HTMLElement)?.blur();
+
+      el.scrollIntoView({ behavior: "smooth" });
+      el.focus();
+      console.log(`Focando em ${sec} (scrollInto)`)
+    }
   }
-  return (
-    <div className="min-h-screen w-full pt-8">
-      <Parallax />
-      <div className="relative z-20 min-h-screen w-full flex flex-col justify-center items-center text-center" id="home">
-        <h1 className="font-bold text-5xl">Let's Sign Up!</h1>
-        <h2 className="font-semibold text-4xl mt-3 w-3/4">Introduce us to yourself with some basic information</h2>
 
-        <button onClick={() => scroll("name")} className="font-semibold text-3xl text-white select-none py-4 px-12 mt-6 border-2 rounded-full bg-black hover-button">
-          Get Started
-        </button>
-  
-      </div>
-      <Name />
-      <Password />
-      <State />
-    </div>
+  // Event handlers
+  function handleKeyboard(e: KeyboardEvent) {
+    // Disable scrolling through keyboard
+    const keysToDisable: Array<string> = [
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+      "PageUp",
+      "PageDown",
+      " ",
+    ];
+
+    if (keysToDisable.includes(e.key)) {
+      e.preventDefault();
+    }
+
+    // If user is typing info, scrolling will stay disabled
+    //if (document.activeElement instanceof HTMLInputElement) {
+    //  return;
+    //}
+
+    // Custom keyboard scrolling
+    if (e.key === "ArrowUp") {
+      console.log("Arrow up")
+      setCurrSec((sec) => (sec > 0 ? sec - 1 : sec));
+    } else if (e.key === "ArrowDown") {
+      console.log("Arrow down")
+      setCurrSec((sec) => (sec + 1 < sections.length ? sec + 1 : sec));
+    }
+  }
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    // Add necessary Event Listener for better UX
+    // - Changes sections through user scroll and keydown
+    //document.addEventListener("scrollend", handleScroll);
+    document.addEventListener("keydown", handleKeyboard);
+    //document.addEventListener("resize", getSectionTops);
+
+    return () => {
+      document.body.style.overflow = "auto";
+
+      //document.removeEventListener("scrollend", handleScroll);
+      document.removeEventListener("keydown", handleKeyboard);
+      //document.removeEventListener("resize", getSectionTops);
+    };
+  }, []);
+
+  // Will scroll offsetY into current section's top
+  useEffect(() => {
+    console.log(`Nova seção ${currSec}`)
+    scrollInto(currSec)
+  }, [currSec]);
+
+  return (
+    <>
+      {/* All other sections */}
+      {sections.map((Section, index) => (
+        <Section.comp
+          key={index}
+          index={index}
+          reference={Section.ref as RefObject<HTMLDivElement>}
+          setSection={setCurrSec}
+        />
+      ))}
+      
+    </>
   );
 }
+
+/** Talvez eu use (espero que não)
+ *   // Gets the Y Offset of all sections
+  function getSectionTops(): Array<number> {
+    const sectionDivs = document.querySelectorAll<HTMLElement>(".section");
+    const tops: Array<number> = Array.from(sectionDivs).map(
+      (section) => section.offsetTop
+    );
+
+    return tops;
+  }
+
+  // This function gets the nearest section based on window Y Offset, so when
+  // user scrolls we can 'end' the task by focusing the nearest section
+  function getNearestTop() {
+    const currTop: number = window.scrollY;
+    const sectionTops: Array<number> = getSectionTops(); // Lembrar de tentar state
+
+    const differences = sectionTops.map((top) => Math.abs(top - currTop));
+  }
+ */
+
+  // Event handlers
+  /*function handleScroll() {
+    console.log("Ativou handle scroll")
+
+    const tops: Array<number | undefined> = sections.map(sec => sec.ref.current?.offsetTop)
+    const differences: Array<number | undefined> = tops.map(top => {
+      if (top === undefined) {
+        return top
+      }
+
+      return Math.abs(window.scrollY - top)
+    })
+
+    const filteredDiffs: Array<number> = differences.filter((diff): diff is number => diff !== undefined);
+
+    if (filteredDiffs.length) {
+      const minDiff: number = Math.min(...filteredDiffs)
+      const nearestRef = differences.indexOf(minDiff)
+
+      setCurrSec(currSec => {
+        if (currSec === nearestRef) {
+          scrollInto(nearestRef)
+        }
+
+        return nearestRef
+      })
+    }
+  }*/
